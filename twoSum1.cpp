0dfe61e03bbf22a79657da172689aead4a3a8d99
SAI PRASAD DAYABARAN <2496saiprasad@gmail.com>
2021-04-23 20:46:51 -0400

    Update twoSum1.cpp

--- twoSum1.cpp
+++ twoSum1.cpp
@@ -5,18 +5,20 @@ public:
         indices.reserve(nums.size());
         
         for(int i=0; i < nums.size(); i++){
-            for(int j=1; j < nums.size(); j++){
+            for(int j=i+1; j < nums.size(); j++){
                 if(nums[j] == (target-nums[i])){
                     //return new int[] {i,j};
                     indices.push_back(i);
                     indices.push_back(j);
                     //break;
+                    goto LABEL;
                 }
-               else
-                   continue;
+              // else
+                //   continue;
             }
-      //  break;
+      
         }
+        LABEL:
         return indices;
     }
 };
